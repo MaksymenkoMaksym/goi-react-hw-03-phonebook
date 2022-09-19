8e8f1bf4222a5caa4a43dcdddd2a86572340f262
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-09-19 21:51:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,32 @@ import InputSearch from './InputSearch/InputSearch';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    try {
+      const parcedConacts = JSON.parse(contacts);
+
+      if (contacts) {
+        this.setState({ contacts: parcedConacts });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentWillUnmount() {}
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState !== this.state) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   onInputContact = user => {
     const { contacts } = this.state;
     if (contacts.some(contact => contact.name === user.name)) {
